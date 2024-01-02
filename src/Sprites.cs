b79fd07d19488a509f9a93d8813ff7228d68f36f
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-01-02 17:20:43 +0200

    Update Sprites.cs

--- src/Sprites.cs
+++ src/Sprites.cs
@@ -13,7 +13,7 @@ namespace Seed
         public int SizeX {get; private set;}
         public int SizeY {get; private set;}
         public string Texture {get; private set;}    
-        public static List<Sprite> Sprites = new List<Sprite>();
+    
 
         public PictureBox PictureBox = new PictureBox();
         public Sprite(int posX, int posY, int sizeX, int sizeY, string texture)
@@ -23,12 +23,34 @@ namespace Seed
             SizeX = sizeX;
             SizeY = sizeY;
             Texture = texture;
-            Image myImage = Image.FromFile(Texture);
+            Image myImage;
+            try
+            {
+                myImage = Image.FromFile(Texture);
+            }
+            catch
+            {
+                myImage = Image.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Textures/NotFound.png"));
+            }
             PictureBox.Location = new Point(posX, posY);
             PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
             PictureBox.Size = new System.Drawing.Size(sizeX, sizeY);
             PictureBox.Image = myImage;
-            Sprites.Add(this);
+            if (GameLogic.isRunning == false)
+            {
+                GameLogic.window.Controls.Add(PictureBox);
+            }
+            else
+            {
+                try
+                {
+                    GameLogic.window.Invoke(() => GameLogic.window.Controls.Add(PictureBox));
+                }
+                catch
+                {
+                    Environment.Exit(0);
+                }
+            }
         }
 
         public void SetPosition(int x, int y)
@@ -44,11 +66,57 @@ namespace Seed
                 Environment.Exit(0);
             }
         }
+        
+        public void SetSize(int x, int y)
+        {
+            try
+            {
+                SizeX = x;
+                SizeY = y;
+                GameLogic.window.Invoke(new Action(() => PictureBox.Size = new Size(PosX, PosY)));
+            }
+            catch
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        public void SetSprite(string Texture)
+        {
+            try
+            {
+                Image myImage = Image.FromFile(Texture);
+                GameLogic.window.Invoke(new Action(() => PictureBox.Image = myImage));
+            }
+            catch(FileNotFoundException)
+            {
+                Image myImage = Image.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Textures/NotFound.png"));
+                GameLogic.window.Invoke(new Action(() => PictureBox.Image = myImage));
+            }
+            catch
+            {
+                Environment.Exit(0);
+            }
+        }
+
 
         public bool IsColliding(Sprite sprite)
         {
             if(this.PosX < sprite.PosX + sprite.SizeX && this.PosX + this.SizeX > sprite.PosX &&
             this.PosY < sprite.PosY + sprite.SizeY && this.PosY + this.SizeY > sprite.PosY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsPointInside(int pointX, int pointY)
+        {
+            if(this.PosX < pointX && this.PosX + this.SizeX > pointX &&
+            this.PosY < pointY && this.PosY + this.SizeY > pointY)
             {
                 return true;
             }
