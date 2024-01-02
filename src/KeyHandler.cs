35618b0bb9876c0b480ed968b341eec4b4e9d875
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-01-02 21:59:26 +0200

    Update KeyHandler.cs

--- src/KeyHandler.cs
+++ src/KeyHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http.Json;
 using System.Reflection.Metadata;
 using System.Windows.Forms;
 using Microsoft.VisualBasic.Devices;
@@ -7,15 +8,27 @@ namespace Seed
 {
     static class KeyHandler
     {
-        public static string CurrentKey {get; set;} = "";
+        public static Dictionary<string, bool> KeysDown {get; private set;} = new Dictionary<string, bool>();
+        
+        static KeyHandler()
+        {
+            foreach(var key in Keys.GetValues(typeof(Keys)))
+            {
+                if(!KeysDown.ContainsKey(Convert.ToString(key)))
+                {
+                    KeysDown.Add(Convert.ToString(key), false);
+                    Console.WriteLine(key);
+                }
+            }
+        }
         public static void KeyDown(object sender, KeyEventArgs e)
         {
-            CurrentKey = Convert.ToString(e.KeyCode);
+            KeysDown[Convert.ToString(e.KeyCode)] = true;            
         }    
 
         public static void KeyUp(object sender, KeyEventArgs e)
         {
-            CurrentKey = string.Empty;
+            KeysDown[Convert.ToString(e.KeyCode)] = false;
         }
    
     }
