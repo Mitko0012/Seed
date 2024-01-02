5bfb48b5d7fa3fbb16b7403ee790e5e7c9efbb76
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-01-02 22:00:25 +0200

    Update Mouse.cs
    Add IsDown property of the mouse class, true if the mouse is down, false if it's up

--- src/Mouse.cs
+++ src/Mouse.cs
@@ -8,11 +8,22 @@ namespace Seed
     {
         public static int MouseX{get; private set;}
         public static int MouseY{get; private set;}
+        public static bool IsDown{get; private set;}
 
         public static void GetMousePos(object sender, MouseEventArgs e)
         {
             MouseX = e.X;
             MouseY = e.Y;
+        }
+
+        public static void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            IsDown = true;
+        }
+
+        public static void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            IsDown = false;
         }
     }
 }
