04f74704b50b959bbb046f9b7194ea02bf4a67ae
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-01-08 16:58:37 +0200

    Update Collider.cs

--- src/Collider.cs
+++ src/Collider.cs
@@ -21,5 +21,61 @@ namespace Seed
             RelativeYEnd = relativeYEnd;
             ParentSprite = sprite;
         }
+        
+        static public bool IsColliding(Sprite sprite, Sprite sprite2)
+        {
+            if(sprite2.PosX < sprite.PosX + sprite.SizeX && sprite2.PosX + sprite2.SizeX > sprite.PosX &&
+            sprite2.PosY < sprite.PosY + sprite.SizeY && sprite2.PosY + sprite2.SizeY > sprite.PosY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static public bool IsColliding(Sprite sprite, Collider collider)
+        {
+            if(sprite.PosX < collider.ParentSprite.PosX + collider.RelativeXEnd && 
+            sprite.PosX + sprite.SizeX > collider.ParentSprite.PosX + collider.RelativeXStart &&
+            sprite.PosY < collider.ParentSprite.PosY + collider.RelativeYEnd && 
+            sprite.PosY + sprite.SizeY > collider.ParentSprite.PosY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static public bool IsColliding(Collider collider, Collider collider2)
+        {
+            if(collider2.ParentSprite.PosX + collider2.RelativeXStart < collider.ParentSprite.PosX + collider.RelativeXEnd && 
+            collider2.ParentSprite.PosX + collider2.RelativeXEnd > collider.ParentSprite.PosX + collider.RelativeXStart &&
+            collider2.ParentSprite.PosY + collider2.RelativeYStart < collider.ParentSprite.PosY + collider.RelativeYEnd && 
+            collider2.ParentSprite.PosX + collider2.RelativeYEnd > collider.ParentSprite.PosY + collider.RelativeYStart)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static public bool IsPointInside(Sprite sprite, int pointX, int pointY)
+        {
+            if(sprite.PosX < pointX && sprite.PosX + sprite.SizeX > pointX &&
+            sprite.PosY < pointY && sprite.PosY + sprite.SizeY > pointY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
