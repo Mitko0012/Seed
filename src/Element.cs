2ee96ab3892a2ae71b46fe04016278aff0d217d4
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-02-02 16:32:43 +0200

    Create Element.cs

---
+++ src/Element.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Seed
+{
+    public class Element
+    {
+        public int PosX;
+        public int PosY;
+        public int RotationCenterX = 0;
+        public int RotationCenterY = 0;
+        public float Angle = 0f;
+        protected int LastDrawnFrame;
+    }
+}
