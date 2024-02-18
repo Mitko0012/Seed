bde3ba34cdeadaa0dd17372da0e3081d556ecbc1
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-02-18 20:56:24 +0200

    Update Window.cs

--- src/Window.cs
+++ src/Window.cs
@@ -9,66 +9,14 @@ namespace Seed
 {
     public class GameWindow : Form
     {  
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        static IntPtr taskBarHandle;
         [DllImport("user32.dll")]
         private static extern IntPtr FindWindow(string className, string windowText);
-        const int hide = 0;
-        const int show = 5;
-        static IntPtr taskBarHandle = FindWindow("Shell_TrayWnd", "");
-        private bool showConsole;
-        public bool ShowConsole {
-            get
-            {
-                return showConsole;
-            }
-            set
-            {
-                var handle = GetConsoleWindow();
-                switch(value)
-                {
-                    case true:
-                        ShowWindow(handle, show);
-                        break;
-                    case false:
-                        ShowWindow(handle, hide);
-                        break;
-                }
-            }
-        }
-        private bool isFullScren;
-        public bool FullScreen {
-        get
-            {
-                return isFullScren;
-            } 
-        set
-            {
-                switch(value)
-                {
-                    case true:
-                        ShowWindow(taskBarHandle, 0);
-                        this.FormBorderStyle = FormBorderStyle.None;
-                        this.WindowState = FormWindowState.Maximized;
-                        this.MaximumSize = this.Size;
-                        this.MinimumSize = this.Size;
-                        break;
-                    case false:
-                        ShowWindow(taskBarHandle, 5);
-                        this.WindowState = FormWindowState.Normal;
-                        this.FormBorderStyle = FormBorderStyle.Sizable;
-                        this.MaximumSize = Size.Empty;
-                        this.MinimumSize = Size.Empty;
-                        break;
-                }
-                isFullScren = value;
-            }
-        }
         public GameWindow(int width, int height)
         {
-            ShowConsole = true;
+            taskBarHandle = FindWindow("Shell_TrayWnd", "");
             Height = height;
             Width = width;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -88,7 +36,7 @@ namespace Seed
         }
         static void Close(object sender, FormClosingEventArgs e)
         {
-            ShowWindow(taskBarHandle, 5);
+            ShowWindow(taskBarHandle, 1);
             Environment.Exit(0);
         }
     }
