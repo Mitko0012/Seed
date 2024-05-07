e724d4fe35dd0186d12f0d1bd3eb2e7b2b22003f
Mitko0012 <152712308+Mitko0012@users.noreply.github.com>
2024-05-07 21:34:54 +0300

    Update ObjectSerialization.cs

--- src/ObjectSerialization.cs
+++ src/ObjectSerialization.cs
@@ -4,8 +4,17 @@ using System.Xml.Serialization;
 
 namespace Seed;
 
+/// <summary>
+/// A class that supports serialization of objects to JSON and XML.
+/// </summary>
 public static class ObjectSerialization
 {
+    /// <summary>
+    /// Serializes an object in JSON and saves it in a file.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="ob">The object to be serialized.</param>
+    /// <param name="filePath">The path where the file will be saved.</param>
     public static void SerializeJsonToFile<T>(T ob, string filePath)
     {
         JsonSerializerOptions options = new JsonSerializerOptions();
@@ -14,12 +23,24 @@ public static class ObjectSerialization
         File.WriteAllText(filePath, json);
     }
 
+    /// <summary>
+    /// Serializes an object to JSON.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="ob">The object to be seiralized.</param>
+    /// <returns>The JSON serialization of the object.</returns>
     public static string SerializeJson<T>(T ob)
     {
         JsonSerializerOptions options = new JsonSerializerOptions();
         options.IncludeFields = true;
         return JsonSerializer.Serialize(ob, options);
     }
+    /// <summary>
+    /// Deserializes an object from a JSON file.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="filePath">The JSON source file.</param>
+    /// <returns>A deserialized object from the JSON source.</returns>
     public static T? DeserializeJsonFromFile<T>(string filePath)
     {
         JsonSerializerOptions options = new JsonSerializerOptions();
@@ -29,6 +50,12 @@ public static class ObjectSerialization
         return ob;
     }
 
+   /// <summary>
+   /// Deserializes an object from JSON.
+   /// </summary>
+   /// <typeparam name="T">The type of the object.</typeparam>
+   /// <param name="json">The JSON to be deserialized.</param>
+   /// <returns>A deserialized object from the JSON source.</returns>
     public static T? DeserializeJson<T>(string json)
     {
         JsonSerializerOptions options = new JsonSerializerOptions();
@@ -37,6 +64,12 @@ public static class ObjectSerialization
         return ob;
     }
 
+    /// <summary>
+    /// Serializes an object in XML and saves it in a file.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="ob">The object to be serialized.</param>
+    /// <param name="filePath">The path where the file will be saved.</param>
     public static void SerializeXmlToFile<T>(T ob, string filePath)
     {
         XmlSerializer ser = new XmlSerializer(typeof(T));
@@ -45,6 +78,12 @@ public static class ObjectSerialization
         write.Close(); 
     }
 
+    /// <summary>
+    /// Serializes an object to XML.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="ob">The object to be serialized.</param>
+    /// <returns>The XML serialization of the object.</returns>
     public static string SerializeXml<T>(T ob)
     {
         XmlSerializer ser = new XmlSerializer(typeof(T));
@@ -54,6 +93,12 @@ public static class ObjectSerialization
         return write.ToString(); 
     }
 
+    /// <summary>
+    /// Deserializes an object from an XML file.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="filePath">The XML source file.</param>
+    /// <returns>A deserialized object from the XML source.</returns>
     public static T? DeserializeXmlFromFile<T>(string filePath)
     {
         XmlSerializer ser = new XmlSerializer(typeof(T));
@@ -62,6 +107,12 @@ public static class ObjectSerialization
         return ob;
     }
 
+    /// <summary>
+    /// Deserializes an object from XML.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="xml">The XML to be deserialized.</param>
+    /// <returns>A deserialized object from the XML source.</returns>
     public static T? DeserializeXml<T>(string xml)
     {
         XmlSerializer ser = new XmlSerializer(typeof(T));
