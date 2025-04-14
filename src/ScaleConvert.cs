21c7e36b6157bd353fb8f6e43fec976c1ecdebb9
Mitko0012 <morkovbul2@gmail.com>
2025-04-14 21:15:15 +0300

    Add documentation for the ScaleConverter class and its methods

--- src/ScaleConvert.cs
+++ src/ScaleConvert.cs
@@ -1,7 +1,18 @@
 namespace Seed;
 
+/// <summary>
+/// A class that contains methods that turn values from position in pixels to position in game units.
+/// </summary>
 public static class ScaleConverter
 {
+    /// <summary>
+    /// Converts a value from pixels to game units.
+    /// </summary>
+    /// <param name="value">The value to be converted.</param>
+    /// <param name="IsPos">True if the value represents a position, false if it represents a scale.</param>
+    /// <param name="IsX">True if the value represents a position on the X axis, false if it represents one on the Y axis.</param>
+    /// <param name="IsSticky">True if the value represents a property of a sticky element, false if not</param>
+    /// <returns>The value in pixels.</returns>
     public static double NeutralToGame(double value, bool IsPos, bool IsX, bool IsSticky)
     {
         double unit = Math.Min(GameLogic.Width, GameLogic.Height) / GameLogic.UnitsOnCanvas;
@@ -19,7 +30,7 @@ public static class ScaleConverter
     }
     
     /// <summary>
-    /// Converts a value from game units to pixels. Used for drawing the elements.
+    /// Converts a value from game units to pixels.
     /// </summary>
     /// <param name="value">The value to be converted.</param>
     /// <param name="IsPos">True if the value represents a position, false if it represents a scale.</param>
